c7b39c9c164118ab1d980a04f6b37d954e2e36ed
Prashik <gamreprashik03@gmail.com>
2025-02-06 07:05:09 +0530

    eslint error solved

--- app/(branding)/page.tsx
+++ app/(branding)/page.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import FadeInWrapper from "@/components/global/FadeInWrapper";
 import HeroSection from "./_components/HeroSection";
 import CardSection from "./_components/CardSection";
 import Footer from "./_components/Footer";
